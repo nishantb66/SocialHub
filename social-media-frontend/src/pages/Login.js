4bbbf266700eb5fef3dd229c62caceae06bc8cee
Nishant Baruah <106482019+nishantb66@users.noreply.github.com>
2025-01-03 00:09:06 +0530

    Update Login.js

--- social-media-frontend/src/pages/Login.js
+++ social-media-frontend/src/pages/Login.js
@@ -16,7 +16,7 @@ function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://social-media-backend-idoy.onrender.com/api/auth/login",
         formData
       );
       localStorage.setItem("token", res.data.token);
