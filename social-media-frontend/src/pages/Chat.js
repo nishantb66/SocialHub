2526212544eac0e61c6cb2348d44f087fb600729
Nishant Baruah <106482019+nishantb66@users.noreply.github.com>
2025-01-03 00:44:25 +0530

    Update Chat.js

--- social-media-frontend/src/pages/Chat.js
+++ social-media-frontend/src/pages/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://social-media-backend-idoy.onrender.com"); // Replace with your backend URL
+const socket = io("https://socialhub-backend-3j7g.onrender.com"); // Replace with your backend URL
 
 function Chat() {
   const [message, setMessage] = useState("");
